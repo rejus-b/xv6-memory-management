dc7f3e2910b46b12608968fc304416597711bf08
Rejus Bulevicius <ed20rb5@leeds.ac.uk>
2022-10-11 20:46:02 +0100

    PIDs

---
+++ user/hw.c
@@ -0,0 +1,13 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]) {
+
+	//insert code below to print 'Hello world' to the console
+	// you can use the printf function as you would normally.
+	// Terminate the process
+	
+	printf("Hello world");
+
+	exit(0);
+}
---
+++ user/sleep.c
@@ -0,0 +1,21 @@
+
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+
+	// If no input
+	if (argc == 1)
+	{
+		printf("Invalid input");	
+	}
+
+	// Convert the time from a string to a number
+	//int time = atoi(argv[2]);	
+	sleep(1);
+	
+	exit(0);
+
+}
+
